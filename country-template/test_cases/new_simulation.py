6bbc7d57fbfd608925d61d6a580a8fdd61c73b03
sandcha <sandcha@users.noreply.github.com>
2020-01-17 22:38:42 +0100

    Remove dead code

--- country-template/test_cases/new_simulation.py
+++ country-template/test_cases/new_simulation.py
@@ -1,4 +1,3 @@
-from openfisca_core import periods
 from openfisca_core.simulation_builder import SimulationBuilder
 from openfisca_country_template import CountryTaxBenefitSystem
 
@@ -26,7 +25,6 @@ tax_benefit_system = CountryTaxBenefitSystem()
 
 simulation_builder = SimulationBuilder()
 simulation = simulation_builder.build_from_entities(tax_benefit_system, TEST_CASE)
-simulation.trace = True
 
 housing_allowance = simulation.calculate('housing_allowance', '2011-01')
 print("housing_allowance", housing_allowance)
