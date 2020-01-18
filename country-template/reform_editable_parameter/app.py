f1d330541df7998257cb2c6d1ba6748234e0f46a
sandcha <sandcha@users.noreply.github.com>
2020-01-18 15:41:40 +0100

    Add dependencies and install+run commands

--- country-template/reform_editable_parameter/app.py
+++ country-template/reform_editable_parameter/app.py
@@ -6,10 +6,12 @@ from openfisca_core.simulation_builder import SimulationBuilder
 from openfisca_country_template import CountryTaxBenefitSystem
 from income_tax_rate_editable_reform import income_tax_rate_editable_reform
 
+
 tax_benefit_system = CountryTaxBenefitSystem()
 period = '2020-01'
 salaries = np.array([1500, 2500, 3500])
 
+# Calculate income tax according to current tax rate
 
 simulation_builder = SimulationBuilder()
 simulation_1 = simulation_builder.build_default_simulation(tax_benefit_system, count=3)
@@ -19,10 +21,11 @@ print("> current income_tax_rate", tax_benefit_system.parameters(period).taxes.i
 income_tax = simulation_1.calculate('income_tax', period)
 print("income_tax", income_tax)
 
+# Calculate income tax according to csv tax rates
 
 csv_data = pandas.read_csv('./income_tax_rates.csv')
 for income_tax_rate in csv_data.income_tax_rate:
-    print("> income_tax_rate", income_tax_rate)
+    print("> reform income_tax_rate", income_tax_rate)
     specific_legislation = income_tax_rate_editable_reform(tax_benefit_system, income_tax_rate)
 
     simulation_builder = SimulationBuilder()
@@ -31,5 +34,3 @@ for income_tax_rate in csv_data.income_tax_rate:
 
     income_tax = simulation_2.calculate('income_tax', period)
     print("income_tax", income_tax)
-
-
