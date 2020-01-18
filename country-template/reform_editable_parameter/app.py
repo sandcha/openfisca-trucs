3b086de8929bf1749e0ca4b7bd63842beb29b1b1
sandcha <sandcha@users.noreply.github.com>
2020-01-18 15:30:14 +0100

    Get income tax rates from csv file

--- country-template/reform_editable_parameter/app.py
+++ country-template/reform_editable_parameter/app.py
@@ -1,32 +1,35 @@
 import numpy as np
+import pandas
 
 from openfisca_core.simulation_builder import SimulationBuilder
 
 from openfisca_country_template import CountryTaxBenefitSystem
 from income_tax_rate_editable_reform import income_tax_rate_editable_reform
 
-
-def new_simulation(tax_benefit_system):
-    simulation_builder = SimulationBuilder()
-    simulation = simulation_builder.build_default_simulation(tax_benefit_system, count=3)
-    simulation.set_input('salary', '2020-01', np.array([1500, 2500, 3500]))
-
-    return simulation
-
 tax_benefit_system = CountryTaxBenefitSystem()
 period = '2020-01'
+salaries = np.array([1500, 2500, 3500])
 
-simulation_1 = new_simulation(tax_benefit_system)
-print("> previous income_tax_rates", tax_benefit_system.parameters.taxes.income_tax_rate)
+
+simulation_builder = SimulationBuilder()
+simulation_1 = simulation_builder.build_default_simulation(tax_benefit_system, count=3)
+simulation_1.set_input('salary', '2020-01', salaries)
+
+print("> current income_tax_rate", tax_benefit_system.parameters(period).taxes.income_tax_rate)
 income_tax = simulation_1.calculate('income_tax', period)
 print("income_tax", income_tax)
 
-for i in range(1, 5):
-    income_tax_rate = i/10
-    print("income_tax_rate", income_tax_rate)
+
+csv_data = pandas.read_csv('./income_tax_rates.csv')
+for income_tax_rate in csv_data.income_tax_rate:
+    print("> income_tax_rate", income_tax_rate)
     specific_legislation = income_tax_rate_editable_reform(tax_benefit_system, income_tax_rate)
 
-    simulation_3 = new_simulation(specific_legislation)
-    print("> new income_tax_rates", specific_legislation.parameters.taxes.income_tax_rate)
-    income_tax = simulation_3.calculate('income_tax', period)
+    simulation_builder = SimulationBuilder()
+    simulation_2 = simulation_builder.build_default_simulation(specific_legislation, count=3)
+    simulation_2.set_input('salary', '2020-01', salaries)
+
+    income_tax = simulation_2.calculate('income_tax', period)
     print("income_tax", income_tax)
+
+
