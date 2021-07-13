12636f0f868efb3352e44a2e1e9b6f50ebd451ee
sandcha <sandcha@users.noreply.github.com>
2021-07-13 16:32:34 +0100

    Explicit that the simulation is calculated for 3 persons

--- country-template/test_cases/explore_parameters.py
+++ country-template/test_cases/explore_parameters.py
@@ -23,11 +23,11 @@ nb_persons = 3
 simulation_builder = SimulationBuilder()
 simulation = simulation_builder.build_default_simulation(tax_benefit_system, count=nb_persons)
 
-# before december 2016, the housing allowance amont is > 0
-simulation.set_input("rent", "2011-01", numpy.array([0, 500, 1000]))
+# before december 2016, the housing allowance amonts are > 0 when the rent is > 0
+simulation.set_input("rent", "2011-01", numpy.array([0, 500, 1000]))  # 3 rents for 3 persons
 housing_allowance = simulation.calculate("housing_allowance", "2011-01")
 print("housing_allowance for active benefits.housing_allowance", housing_allowance)
 
-# from # before december 2016, the housing allowance amont is 0
+# from # before december 2016, the housing allowance amonts are 0
 housing_allowance_ended_parameter = simulation.calculate("housing_allowance", "2016-12")
 print("housing_allowance_ended_parameter", housing_allowance_ended_parameter)
