4a6e52467c9caf92fe82a9663af2fe920187ff71
sandcha <sandcha@users.noreply.github.com>
2021-09-08 18:29:42 +0200

    Update France simulation default date to 2021

--- openfisca-france/new_simulation.py
+++ openfisca-france/new_simulation.py
@@ -7,5 +7,6 @@ tbs = FranceTaxBenefitSystem()
 sb = SimulationBuilder()
 simulation = sb.build_default_simulation(tbs, count=3)
 
-period = '2020-01'
-print(simulation.calculate('smic_proratise', period))
+period = '2021-10'
+smic_proratise = simulation.calculate('smic_proratise', period)
+print("smic_proratise :", smic_proratise)  # 1554.6174
