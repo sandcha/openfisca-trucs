10d8c21c9799b77b899afc1d1d9a3120bda88a00
sandcha <sandcha@users.noreply.github.com>
2024-12-08 17:07:28 +0100

    Extract smic check from France simulation script
    And send first commit from free air france wifi :)

--- openfisca-france/new_simulation.py
+++ openfisca-france/new_simulation.py
@@ -1,12 +1,9 @@
 from openfisca_core.simulation_builder import SimulationBuilder
 from openfisca_france import FranceTaxBenefitSystem
-
+# from openfisca_core.model_api import ADD
 
 tbs = FranceTaxBenefitSystem()
 
+nombre_entites = 3
 sb = SimulationBuilder()
-simulation = sb.build_default_simulation(tbs, count=3)
-
-period = '2021-10'
-smic_proratise = simulation.calculate('smic_proratise', period)
-print("smic_proratise :", smic_proratise)  # 1554.6174
+simulation = sb.build_default_simulation(tbs, count=nombre_entites)
